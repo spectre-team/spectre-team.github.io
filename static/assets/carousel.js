365e1b832d6425ab64649834fc7aed10f59e38e5
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-04-25 12:59:39 +0200

    Add logo slider generator

---
+++ static/assets/carousel.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.friends-logos').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 1
+      }
+    }]
+  });
+});
